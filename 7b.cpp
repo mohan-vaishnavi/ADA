34a96614291fc9bab5c5728e7df00508100c5cb9
mohan-vaishnavi <46642908+mohan-vaishnavi@users.noreply.github.com>
2019-10-12 10:43:25 +0530

    Update 7b.cpp

--- 7b.cpp
+++ 7b.cpp
@@ -1,33 +1,39 @@
 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include<stdio.h> 
 
 
 int max(int a, int b) { return (a > b)? a : b; } 
 
+
 int knapSack(int W, int wt[], int val[], int n) 
 { 
-	
+int i, w; 
+int K[n+1][W+1]; 
 
-if (n == 0 || W == 0) 
-	return 0; 
 
-if (wt[n-1] > W) 
-	return knapSack(W, wt, val, n-1); 
-
-else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), 
-					knapSack(W, wt, val, n-1) ); 
+for (i = 0; i <= n; i++) 
+{ 
+	for (w = 0; w <= W; w++) 
+	{ 
+		if (i==0 || w==0) 
+			K[i][w] = 0; 
+		else if (wt[i-1] <= w) 
+				K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]); 
+		else
+				K[i][w] = K[i-1][w]; 
+	} 
 } 
 
+return K[n][W]; 
+} 
 
 int main() 
 { 
-	int val[] = {60, 100, 120}; 
+	int val[] = {70, 80, 40}; 
 	int wt[] = {10, 20, 30}; 
 	int W = 50; 
 	int n = sizeof(val)/sizeof(val[0]); 
-	cout<<knapSack(W, wt, val, n); 
+	printf("%d", knapSack(W, wt, val, n)); 
+	printf("\n");
 	return 0; 
 } 
-
-
